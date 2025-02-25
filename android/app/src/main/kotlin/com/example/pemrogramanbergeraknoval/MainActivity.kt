f9e6536e2ba99a1c443e571956f1d3911153b9a7
NovalAdiyaPutra <12350112933@students.uin-suska.ac.id>
2025-02-25 15:48:51 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pemrogramanbergeraknoval/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pemrogramanbergeraknoval
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/tugas1.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Halamanpertama(),
+        debugShowCheckedModeBanner: false
+    );
+  }
+}
+
+class Halamanpertama extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Nama: Noval Adiya Putra",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Halamankedua()),
+                );
+              },
+              child: Text("Ke Halaman Berikutnya"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Halamankedua extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "NIM: 12350112933",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Kembali ke Halaman Utama"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
